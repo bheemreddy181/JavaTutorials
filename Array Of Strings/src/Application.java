826c8eaec2096b1412b9e34b597fe861452e5f3e
CEA <CEA@DESKTOP-J9S6P4G>
2016-10-31 12:14:56 +0200

    first commit

---
+++ Array Of Strings/src/Application.java
@@ -0,0 +1,24 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		String [] words = new String[3];
+		
+		words[0]="Hello";
+		words[1]="to";
+		words[2]="you";
+	
+		
+		for(String word:words){
+			
+			System.out.println(word);
+		}
+
+		String[] fruits = {"apple","banana","pear","kiwi"};
+		
+		for(String fruit:fruits){
+			
+			System.out.println(fruit);
+		}
+	}
+}
