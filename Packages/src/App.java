e9869d4af5b2db80c427455a5af92a76bd4eadef
CEA <CEA@DESKTOP-J9S6P4G>
2016-10-31 15:18:10 +0200

    Packages

---
+++ Packages/src/App.java
@@ -0,0 +1,17 @@
+import ocean.Fish;
+import ocean.plants.Algae;
+import ocean.plants.Seaweed;
+
+// or we can use ocean.* to import all ocean package
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Fish fish =new Fish();
+		Seaweed weed = new Seaweed();
+		Algae alea = new Algae();
+	}
+
+}
---
+++ Packages/src/emre/oceangame/Aquarium.java
@@ -0,0 +1,5 @@
+package emre.oceangame;
+
+public class Aquarium {
+
+}
