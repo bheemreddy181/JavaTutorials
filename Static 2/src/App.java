43f1fd50c1c163f263f281f8a35bd84ab69f4351
CEA <CEA@DESKTOP-J9S6P4G>
2016-11-02 19:57:01 +0200

    Static to count how many object is created from a class

---
+++ Static 2/src/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s1 = new Student();
+		Student s2 = new Student();
+		
+		System.out.println(Student.getNoOfStudents());
+	}
+
+}
---
+++ Static 2/src/Student.java
@@ -0,0 +1,31 @@
+
+public class Student {
+
+	String name;
+	int age;
+	static int noOfStudents = 0;
+	
+	Student(){
+		noOfStudents++;
+	}
+	
+	public static int getNoOfStudents(){
+		return noOfStudents;
+	}
+	public String getName(){
+		return name;
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
